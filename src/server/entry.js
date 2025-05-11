671ee3d49308564cbb4acc897101280b3ab400e2
Robert Hollencamp <rhollencamp@gmail.com>
2025-05-11 00:05:47 -0400

    use connect for middlewares, calling vite middlewares directly was causing problems that we don't need to spend time on

--- src/server/entry.js
+++ src/server/entry.js
@@ -1,10 +1,11 @@
-import { readFileSync } from "node:fs";
+import connect from "connect";
+import http from "node:http";
+import { WebSocketServer } from "ws";
+import { createServer as createViteServer } from "vite";
 import { dirname, resolve } from "node:path";
 import { fileURLToPath } from "node:url";
-import { createServer as createViteServer } from "vite";
-import { WebSocketServer } from "ws";
-import { createServer as createHttpServer } from "node:http";
 import { handleWebsocketConnection } from "./netcode.js";
+import { readFileSync } from "node:fs";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
@@ -14,28 +15,30 @@ const vite = await createViteServer({
   appType: "custom",
 });
 
-// create nodejs server
-const server = createHttpServer((req, res) => {
-  // use vite middleware
-  vite.middlewares(req, res, async () => {
-    const url = req.url;
+// http server
+const app = connect();
+app.use(vite.middlewares);
+app.use(async (req, res) => {
+  if (req.url !== "/") {
+    res.writeHead(404);
+    res.end();
+    return;
+  }
 
-    // TODO check path and 404 if not /
+  // read index.html
+  let template = readFileSync(
+    resolve(__dirname, "../client/index.html"),
+    "utf-8",
+  );
 
-    // read index.html
-    let template = readFileSync(
-      resolve(__dirname, "../client/index.html"),
-      "utf-8",
-    );
+  // run it through vite
+  template = await vite.transformIndexHtml(req.url, template);
 
-    // run it through vite
-    template = await vite.transformIndexHtml(url, template);
-
-    // send response
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.end(template);
-  });
+  // send response
+  res.writeHead(200, { "Content-Type": "text/html" });
+  res.end(template);
 });
+const server = http.createServer(app);
 server.listen(8080);
 
 // create websocket server on the same port
